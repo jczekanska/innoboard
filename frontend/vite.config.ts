3fd89e067bfa0de503a6320904e19c7611320438
Emanuel Borges <ebbmango@tuta.io>
2025-05-12 20:28:55 +0200

    set up shadcn

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,14 +1,21 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import path from "path";
+import tailwindcss from "@tailwindcss/vite";
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), tailwindcss()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
   server: {
     proxy: {
-      '/api': {
-        target: 'http://backend:8000',
+      "/api": {
+        target: "http://backend:8000",
         changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, ''),
+        rewrite: (path) => path.replace(/^\/api/, ""),
       },
     },
   },
